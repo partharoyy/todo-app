af702c8eaec7847e013c9eb5e5f88983fb2a5acb
partharoyy <partharoyy88@gmail.com>
2021-07-29 17:35:08 +0530

    disabled firebase key for now

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCv8MxsyjmAgKQ7fdfSH_aQNTWG1k72HoM",
-    authDomain: "todo-app-79226.firebaseapp.com",
-    projectId: "todo-app-79226",
-    storageBucket: "todo-app-79226.appspot.com",
-    messagingSenderId: "1013742479335",
-    appId: "1:1013742479335:web:d6dd80d2bd17673b36bf42"
-};
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCv8MxsyjmAgKQ7fdfSH_aQNTWG1k72HoM",
+//     authDomain: "todo-app-79226.firebaseapp.com",
+//     projectId: "todo-app-79226",
+//     storageBucket: "todo-app-79226.appspot.com",
+//     messagingSenderId: "1013742479335",
+//     appId: "1:1013742479335:web:d6dd80d2bd17673b36bf42"
+// };
 
 firebase.initializeApp(firebaseConfig);
 
